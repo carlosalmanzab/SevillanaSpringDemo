1b7ae60eeaffb594cb4ce34758b6b05522dc60e9
Carlos AB <64765773+carlosalmanzab@users.noreply.github.com>
2023-01-28 23:02:49 -0500

    add main

---
+++ src/main/java/com/sevillana/SevillanaSpringDemo1Application.java
@@ -0,0 +1,13 @@
+package com.sevillana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevillanaSpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevillanaSpringDemo1Application.class, args);
+	}
+
+}
